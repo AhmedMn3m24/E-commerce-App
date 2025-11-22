6b7c80317dcd75b8c22a269bdbb51b59c19834c9
Ahmed <aabdelmonem863@gmail.com>
2025-11-22 07:02:52 +0200

    Deleted payment and notfound pages

--- src/app/NoFound/notfound.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function NotFound() {
-  return (
-    <div className="flex justify-center mt-20">
-      <Image src="/images/error.svg" alt="Error" width={300} height={300} />
-    </div>
-  );
-}
