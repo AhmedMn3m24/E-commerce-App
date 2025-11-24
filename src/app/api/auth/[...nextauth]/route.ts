48467450fae39fc4973abea53a70f9de4853cca1
Ahmed <aabdelmonem863@gmail.com>
2025-11-24 22:56:28 +0200

    fix type S

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,121 +1,5 @@
 
 
-
-
-
-// // "use server";
-// import NextAuth, { AuthOptions, DefaultSession } from "next-auth";
-// import CredentialsProvider from "next-auth/providers/credentials";
-// import { jwtDecode } from "jwt-decode";
-
-// declare module "next-auth" {
-//   interface Session {
-//     user: {
-//       id?: string;
-//       credentialsToken?: string;
-//     } & DefaultSession["user"];
-//   }
-
-//   interface User {
-//     id?: string;
-//     credentialsToken?: string;
-//   }
-
-//   interface JWT {
-//     credentialsToken?: string;
-//     userId?: string;
-//     name?: string;
-//   }
-// }
-
-// export const NextAuthConfig: AuthOptions = {
-//   providers: [
-//     CredentialsProvider({
-//       name: "credentials",
-//       credentials: {
-//         email: { label: "Email", type: "text" },
-//         password: { label: "Password", type: "password" },
-//       },
-//       async authorize(credentials) {
-//         try {
-//           const res = await fetch(
-//             "https://ecommerce.routemisr.com/api/v1/auth/signin",
-//             {
-//               method: "POST",
-//               headers: { "Content-Type": "application/json" },
-//               body: JSON.stringify({
-//                 email: credentials?.email,
-//                 password: credentials?.password,
-//               }),
-//             }
-//           );
-
-//           const finalRes = await res.json();
-//           console.log("finalRes auth", finalRes);
-
-//           if (finalRes.message === "success") {
-//             const decodedToken: {
-//               _id: string;
-//               email: string;
-//               name: string;
-//             } = jwtDecode(finalRes.token);
-
-//             return {
-//               id: decodedToken._id,
-//               name: decodedToken.name,
-//               email: decodedToken.email,
-//               credentialsToken: finalRes.token,
-//             };
-//           }
-
-//           return null;
-//         } catch (error) {
-//           console.error("Authorize error:", error);
-//           return null;
-//         }
-//       },
-//     }),
-//   ],
-
-//   pages: {
-//     signIn: "/register",
-//   },
-
-//   callbacks: {
-//     async jwt({ token, user }) {
-//       if (user) {
-//         const decodedToken: any = jwtDecode(user.credentialsToken!);
-
-//         token.credentialsToken = user.credentialsToken;
-//         token.userId = decodedToken._id;
-//         token.name = decodedToken.name;
-//       }
-//       return token;
-//     },
-
-//     async session({ session, token }) {
-//       if (!session.user) session.user = {} as any;
-//       // @ts-ignore
-
-//       session.user.id = token.userId;
-//       session.user.name = token.name;
-//       // @ts-ignore
-
-//       session.user.credentialsToken = token.credentialsToken;
-
-//       return session;
-//     },
-//   },
-
-//   session: {
-//     strategy: "jwt",
-//     maxAge: 3 * 30 * 24 * 60 * 60,
-//   },
-// };
-
-// const handler = NextAuth(NextAuthConfig);
-// export { handler as GET, handler as POST };
-
 "use server";
 
 import NextAuth, { AuthOptions, DefaultSession } from "next-auth";
@@ -195,7 +79,7 @@ export const NextAuthConfig: AuthOptions = {
   ],
 
   pages: {
-    signIn: "/login", // صفحة تسجيل دخول فعلية
+    signIn: "/login",
   },
 
   callbacks: {
@@ -211,7 +95,6 @@ export const NextAuthConfig: AuthOptions = {
     async session({ session, token }) {
       session.user = session.user || {} as any;
 
-      // Type assertions عشان TypeScript يبني
       session.user.id = token.userId as string | undefined;
       session.user.name = token.name as string | undefined;
       session.user.credentialsToken = token.credentialsToken as string | undefined;
@@ -222,7 +105,7 @@ export const NextAuthConfig: AuthOptions = {
 
   session: {
     strategy: "jwt",
-    maxAge: 3 * 30 * 24 * 60 * 60, // 3 شهور
+    maxAge: 3 * 30 * 24 * 60 * 60,
   },
 };
 
